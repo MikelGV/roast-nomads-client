9ea20254fb5ac3021bc82150d70d42a60f0c2f62
MikelGV <mikelgaldosv@gmail.com>
2025-04-02 02:31:32 +0200

    FEATURE: login and signup pages created

---
+++ src/app/_components/topBar.tsx
@@ -0,0 +1,9 @@
+import Link from "next/link";
+
+export function TopBar() {
+    return (
+        <div className="">
+            <Link href="/login" className="text-[#D97430]">Try Demo</Link>
+        </div>
+    )
+}
---
+++ src/app/login/page.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+import Link from "next/link";
+
+
+export default function LogIn() {
+    return (
+        <div className="flex justify-center items-center flex-row w-full h-screen gap-10">
+            <div className="">
+                <Image src="" alt="image missing" className=""/>
+            </div>
+            <div className="bg-[#FFFF] shadow-lg rounded-md px-8 pt-6 pb-8 mb-4">
+                <h2 className="text-4xl text-[#5C3A1A] p-8 font-bold">Welcome Back Nomad</h2>
+                <form className="flex flex-col items-center">
+                    <input className="border-2 border-[#5C3A1A] rounded-lg text-2xl p-3 mb-10 placeholder-[#5C3A1A]" name="email" placeholder="Email"/>
+                    <input className="border-2 border-[#5C3A1A] rounded-lg text-2xl p-3 mb-10 placeholder-[#5C3A1A]" name="password" placeholder="Password"/>
+                    <button className="bg-[#5C3A1A] text-[#F5F2EE] rounded-lg p-3 px-24 text-2xl mb-10">Log In</button>
+                    <Link href="/signup" className="text-[#227DA3] text-2xl mb-10">Join Now</Link>
+                </form>
+            </div>
+        </div>
+    )
+}
