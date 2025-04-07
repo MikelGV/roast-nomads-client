8915f7c2ec0791f94e88acb3fb071b13720ef2a3
MikelGV <mikelgaldosv@gmail.com>
2025-04-07 11:01:03 +0200

    FEATURE: most of the home page done, masonry grid needed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,43 +1,41 @@
 import Link from "next/link";
 
-import { api, HydrateClient } from "~/trpc/server";
+import {  HydrateClient } from "~/trpc/server";
 import { TopBar } from "./_components/topBar";
 
 export default async function Home() {
-  const hello = await api.post.hello({ text: "from tRPC" });
 
-  void api.post.getLatest.prefetch();
 
   return (
     <HydrateClient>
       <main className="flex min-h-screen flex-col items-center justify-center ">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-            <TopBar />
+                    {/*<TopBar />*/}
             {/* First part*/}
             <div className="">
-                <h1 className="text-6xl font-bold text-[#5C3A1A]">Travel the World Through Coffee</h1>
+                <h1 className="text-6xl font-bold text-center text-[#5C3A1A]">Travel the World Through Coffee</h1>
                 <h2 className="text-xl text-center text-[#5C3A1A]">Discover specialty roasters from around the globe, curated just for you</h2>
-                <div className="">
-                    <img src="" />
-                    <Link href="">Start your adventure</Link>
+                <div className="relative w-screen h-[47rem]">
+                    <img src="/images/ChatGPT-Image-Apr-1,2025,01_39_14-AM.png" className="w-full h-full object-cover"/>
+                    <Link href="" className="absolute bottom-36 left-52 bg-[#D97430] text-xl font-bold text-white px-20 py-6 rounded-lg">Start your adventure</Link>
                 </div>
             </div>
             {/*Second part*/}
             <div className="p-2 container mx-auto">
                 <h1 className="text-4xl font-bold text-[#5C3A1A] mb-8">Your Coffe Journey Begins Here</h1>
 
-                <div className="flex items-center mb-12 gap-6">
+                <div className="flex mb-12 gap-6">
                     <div className="flex flex-col gap-6">
                         <p className="text-xl text-semibold bg-white p-8 shadow-md w-96">Step 1: Tell us your tastes.</p>
                         <span className="text-xl mb-6 text-[#5C3A1A] w-96">Quick preferences quiz to discover your ideal roast.</span>
                     </div>
-                    <div className="">
-                        <img src="https://images.pexels.com/photos/851555/pexels-photo-851555.jpeg?auto=compress&cs=tinysrgb&w=600" />
+                    <div className="flex items-center">
+                        <img src="/images/pexels-jayoke-851555.jpg" className="w-[60rem] h-[27rem] object-cover" />
                     </div>
                 </div>
-                <div className="flex items-center mb-12 justify-betweeen">
-                    <div className="">
-                        <img src="https://via.placeholder.com/400x300" />
+                <div className="flex mb-12 justify-betweeen">
+                    <div className="mr-10 ">
+                        <img src="/images/pexels-cottonbro-4820817.jpg" className="w-[59rem] h-[27rem] object-cover" />
                     </div>
                     <div className="flex flex-col gap-6">
                         <p className="text-xl text-semibold bg-white p-8 shadow-md w-96">Step 2: We curate your coffee</p>
@@ -46,28 +44,33 @@ export default async function Home() {
                 </div>
                 <div className="flex items-center justify-betweeen mb-12 justify-betweeen">
                     <div className="flex flex-col gap-6">
-                        <p className="text-xl text-semibold bg-white p-8 shadow-md w-96">Step 3: Explore and Enjoy</p>
+                        <p className="text-xl text-semibold bg-white p-8 shadow-md w-96 mr-10">Step 3: Explore and Enjoy</p>
                         <span className="text-xl text-[#5C3A1A] w-96">Share your coffee adventures with a trhiving community.</span>
                     </div>
                     <div className="">
-                        <img src="https://via.placeholder.com/400x300" />
+                            <img src="/images/pexels-enginakyurt-2456429.jpg" className="w-[59rem] h-[27rem] object-cover" />
                     </div>
                 </div>
             </div>
             {/*Third part*/}
             <div className="p-2">
-                <h1 className="text-4xl font-bold text-[#5C3A1A]">Join Our Global Coffee Community</h1>
-                <img src="" />
-                <div className="">
-                    <h2 className="text-3xl font-bold text-[#5C3A1A]"> Coffee That Makes a Difference</h2>
-                    <span className="text-xl text-[#5C3A1A]">
-                        We partener with roasters committed to sustainable sourcing,
-                        ethical practices, and exceptional quality-so every cup you
-                        brew supports both people and planet.
-                    </span>
-                    <img src="" className=""/>
-                    <img src="" className=""/>
-                    <img src="" className=""/>
+                <h1 className="text-4xl font-bold text-[#5C3A1A] mb-3">Join Our Global Coffee Community</h1>
+                <p className="text-xl mb-6 text-[#5C3A1A]">Connect with fellow coffee lovers, discover unique stories behind every roast, and share your own brewing adventures</p>
+                <img src="/images/pexels-valeriiamiller-3361170.jpg" className="w-full h-[40rem] object-cover mb-10"/>
+                <div className="grid grid-cols-2 items-center justify-center">
+                    <div className="h-29 w-[30rem] col-span-1 flex flex-col">
+                        <h2 className="text-3xl font-bold text-[#5C3A1A]"> Coffee That Makes a Difference</h2>
+                        <span className="text-xl text-[#5C3A1A] mb-10">
+                            We partener with roasters committed to sustainable sourcing,
+                            ethical practices, and exceptional quality-so every cup you
+                            brew supports both people and planet.
+                        </span>
+                        <div className="">
+                        <img src="/images/pexels-michael-burrows-7125739.jpg" className="w-[40rem] h-[20rem] object-cover"/>
+                        </div>
+                    </div>
+                    <img src="/images/pexels-nsmedia97-27777799.jpg" className="w-[46rem] h-[30rem] object-cover col-span-1 mt-[30rem]"/>
+                    <img src="/images/pexels-michael-burrows-7125543.jpg" className="w-[96rem] h-[30rem] object-cover col-span-2 mt-20"/>
                 </div>
             </div>
             {/*Fourth part*/}
@@ -80,10 +83,10 @@ export default async function Home() {
                 </span>
             </div>
             <div className="p-2">
-                <div className="flex items-center">
-                <Link href="" className="bg-[#D97430] p-8 rounded-xl text-center w-96 text-xl text-white">Subscribe now</Link>
+                <div className="flex items-center justify-center mb-10">
+                    <Link href="" className="bg-[#D97430] p-8 rounded-xl text-center w-96 text-xl text-white">Subscribe now</Link>
                 </div>
-                <img src="" />
+                <img src="/images/pexels-helder-quiala-434379851-15294461.jpg" className="w-[96rem] h-[44rem] object-cover" />
             </div>
             {/*Fived part*/}
             <div className="flex flex-col justify-center items-center gap-5">
